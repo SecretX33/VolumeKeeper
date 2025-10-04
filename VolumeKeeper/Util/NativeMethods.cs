ffa68419c488206f38c9c08ac4ed483bcfe1c638
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2025-10-04 12:34:03 -0300

    Fix application icons not being displayed

--- VolumeKeeper/Util/NativeMethods.cs
+++ VolumeKeeper/Util/NativeMethods.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Drawing;
 using System.Runtime.InteropServices;
 using Microsoft.UI.Xaml;
 using WinRT.Interop;
@@ -8,9 +7,6 @@ namespace VolumeKeeper.Util;
 
 internal static partial class NativeMethods
 {
-    [LibraryImport("shell32.dll", EntryPoint = "ExtractIconW", StringMarshalling = StringMarshalling.Utf16)]
-    private static partial IntPtr ExtractIcon(IntPtr hInst, string lpszExeFileName, int nIconIndex);
-
     [LibraryImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
     private static partial bool ShowWindow(IntPtr hWnd, ShowWindowCommand nCmdShow);
@@ -33,13 +29,6 @@ internal static partial class NativeMethods
         SW_RESTORE = 9,
         SW_SHOWDEFAULT = 10,
         SW_FORCEMINIMIZE = 11,
-    }
-
-    public static Icon? ExtractIconFromFile(string filePath)
-    {
-        IntPtr hIcon = ExtractIcon(IntPtr.Zero, filePath, 0);
-        if (hIcon == IntPtr.Zero) return null;
-        return Icon.FromHandle(hIcon);
     }
 
     public static void ShowAndFocus(Window window) => ShowAndFocus(WindowNative.GetWindowHandle(window));
